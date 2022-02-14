734d6ffa274d130b6a1aeb041a4234aadcc2a57a
Paulo <paulo_piotto@hotmail.com>
2022-02-14 15:48:57 -0300

    preparando revisão

--- src/schemas/movementSchema.js
+++
@@ -1,9 +0,0 @@
-import joi from 'joi';
-
-const movementSchema = joi.object({
-  value: joi.number().positive().required(),
-  description: joi.string().required(),
-  date: joi.date().required(),
-  isOutput: joi.boolean().required(),
-});
-export default movementSchema;
